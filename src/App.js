f47e40b3ff02113b71dcd75b74e6b3db41b065a6
Sibusiso <sibusisok59@gmail.com>
2024-08-28 08:59:39 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Signup from './Signup';
-import Login from './Login';
 import Home from './Home';
 import './App.css';
 
@@ -9,9 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
